966b05d660b1fca7c14cd7bcec13d4bd98baa46e
Erbekov Bobur <erbekovbobur66@gmail.com>
2022-10-10 17:27:28 +0500

    Fifth task done

--- script.js
+++ script.js
@@ -11,3 +11,13 @@
 //     return 0;
 // }
 // console.log(compareStr(one, two));
+
+// -- 5  ------------------------------------------------------------------
+
+// function truncate(str, number) {
+//     console.log(str.length);
+//     let result = str.slice(0, number-3 > 0 ? number - 3: 0);
+//     return number-3 < str.length? result + '...': result
+// };
+
+// console.log(truncate(one,6));
