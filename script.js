0ded54ccc2fd222e08104a2ad8ffd0223a70582a
Erbekov Bobur <erbekovbobur66@gmail.com>
2022-10-10 17:22:14 +0500

    Third task done

--- script.js
+++ script.js
@@ -11,3 +11,31 @@
 //     return 0;
 // }
 // console.log(compareStr(one, two));
+//--2-------------------------------------------------------------------
+
+
+// let str = 'hello, my friend';
+
+// function upperFirstSymbol(srting) {
+//     return str[0].toUpperCase() + str.slice(1);
+// }
+
+// console.log(upperFirstSymbol(str));
+
+
+//--3--------------------------------------------------------------------
+
+
+// function calcVowels(str) {
+//     let vowels = 'aeiouyAEIOUY';
+//     let count = 0;
+//     for (let prop in vowels)
+//         for (let key in str) {
+//             if (str[key] === vowels[prop]){
+//                 console.log(`${str[key]} + ${vowels[prop]}`);
+//                 count++;
+//             }
+//         }
+//     return count;
+// }
+// console.log(calcVowels(str));
