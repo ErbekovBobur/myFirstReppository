22dec0957809c489a8245a406d9f9d55ec8cba5a
Erbekov Bobur <erbekovbobur66@gmail.com>
2022-10-10 17:23:48 +0500

    Fourth task done

--- script.js
+++ script.js
@@ -11,3 +11,18 @@
 //     return 0;
 // }
 // console.log(compareStr(one, two));
+
+//--4--------------------------------------------------------------------
+
+
+// let one = 'Получайте скидку только сегодня!';
+// let spam = ['100% бесплатно', 'увеличение продаж', 'только сегодня', 'не удаляйте', 'xxx'];
+
+
+// function checkSpam(str) {
+//     for (let key in spam) {
+//         if(str.indexOf(spam[key]) !== -1) return 'Спам';
+//     }
+//     return 'Не спам';
+// }
+// console.log(checkSpam(one));
