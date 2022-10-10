f2b7d1dc0a9551abe67189961feb0cbec8c7e132
Erbekov Bobur <erbekovbobur66@gmail.com>
2022-10-10 17:32:46 +0500

    Sixth task done

--- script.js
+++ script.js
@@ -11,3 +11,17 @@
 //     return 0;
 // }
 // console.log(compareStr(one, two));
+
+
+//--- 6 -------------------------------------------------------------------
+
+// function palindrom(str) {
+//     for(let i = 0; i < str.length-1; i++){
+//         if (str[i] !== str[(str.length-1)-i]) return false;
+//     }
+//     return true;
+// };
+
+let two = 'ароза упал а н а лапу азора';
+
+// console.log(palindrom(two));
