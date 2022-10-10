e18502f9f1841fe08019cbd6ce3e0b2bb1fe3c3d
Erbekov Bobur <erbekovbobur66@gmail.com>
2022-10-10 17:18:18 +0500

    first task done

---
+++ script.js
@@ -0,0 +1,13 @@
+//--1------------------------------------------------------------------
+
+
+// let one = 'I live in Uzbekistan';
+// let two = `Hi, how are you?`;
+// let two = one;
+
+// function compareStr(str1, str2) {
+//     if(str1.length > str2.length) return 1;
+//     if(str1.length < str2.length) return-1;
+//     return 0;
+// }
+// console.log(compareStr(one, two));
